fa2c446a83ccc9e2fcd210ef7903a10c4cdcf705
Alexey <karpechenko.lesha@gmail.com>
2019-09-21 12:10:39 +0300

    working app!

--- app/src/main/java/com/example/anroidproject/MainActivity.kt
+++ app/src/main/java/com/example/anroidproject/MainActivity.kt
@@ -7,23 +7,24 @@ import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
-    var buttonСhet: Button = findViewById(R.id.button_chet)
-    var buttonNechet: Button = findViewById(R.id.button_nechet)
     var randomNumber = 0
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        val buttonСhet: Button = findViewById(R.id.button_chet)
+        val buttonNechet: Button = findViewById(R.id.button_nechet)
+
         buttonСhet.setOnClickListener{
             randomNumber = (0..10).shuffled().first()
-            resultNumber.setText(randomNumber)
+            resultNumber.setText(" " + randomNumber)
             if (randomNumber % 2 == 0) result.text = "Вы выиграли!"
             else result.text = "Вы проиграли"
         }
         buttonNechet.setOnClickListener{
             randomNumber = (0..10).shuffled().first()
-            resultNumber.setText(randomNumber)
+            resultNumber.setText(" " + randomNumber)
             if (randomNumber % 2 == 1) result.text = "Вы выиграли!"
             else result.text = "Вы проиграли"
         }
